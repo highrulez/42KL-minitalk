536281769dca76138230a2759f687e6e623e479e
Neal Caffrey <42662415+highrulez@users.noreply.github.com>
2023-12-10 13:47:44 +0800

    Update minitalk.h

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: aawgku-o <aawgku-o@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 19:27:20 by aawgku-o          #+#    #+#             */
-/*   Updated: 2023/12/09 19:47:22 by aawgku-o         ###   ########.fr       */
+/*   Updated: 2023/12/10 13:44:54 by aawgku-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 # include <unistd.h>
 # include "./ft_printf/header/ft_printf.h"
 
-void	ft_bit_write(int x);
+void	ft_bitrary(int x);
 void	ft_signal_control(int x);
-void	ft_send_signal(int pid, char *str);
+void	ft_signal(int pid, char *str);
 
 #endif
